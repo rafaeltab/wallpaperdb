59348cd4657d516cd15b47042a40f3a7bf9a15a1
Zero Two <zero.two.rafaeltab@gmail.com>
2026-09-26 18:14:41 +0000

    fix(tags): preserve successful exit status on graceful shutdown

--- apps/tags/src/index.ts
+++ apps/tags/src/index.ts
@@ -1,5 +1,10 @@
 import * as NodeRuntime from '@effect/platform-node-shared/NodeRuntime';
-import { Effect, Logger } from 'effect';
+import { Cause, Effect, Exit, Logger, Runtime } from 'effect';
 import { tagsProgram } from './bootstrap.js';
 
-NodeRuntime.runMain(tagsProgram.pipe(Effect.provide(Logger.layer([Logger.consoleJson]))));
+NodeRuntime.runMain(tagsProgram.pipe(Effect.provide(Logger.layer([Logger.consoleJson]))), {
+  teardown: (exit, onExit) => {
+    if (Exit.isFailure(exit) && Cause.hasInterruptsOnly(exit.cause)) onExit(0);
+    else Runtime.defaultTeardown(exit, onExit);
+  },
+});
